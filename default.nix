b08df170da444e00a70900a7e4236e70547fe8c3
Akira Komamura <akira.komamura@gmail.com>
2020-12-05 15:25:37 +0900

    Add flake.nix

--- default.nix
+++ default.nix
@@ -1,46 +1,9 @@
-{ compiler ? "ghc884" }:
-let
-  sources = import ./nix/sources.nix;
-  pkgs = import sources.nixpkgs { };
-
-  gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-
-  myHaskellPackages = pkgs.haskell.packages.${compiler}.override {
-    overrides = hself: _hsuper: {
-      "epub2json" =
-        hself.callCabal2nix
-          "epub2json"
-          (gitignore ./.)
-          { };
-    };
-  };
-
-  shell = myHaskellPackages.shellFor {
-    packages = p: [
-      p."epub2json"
-    ];
-    buildInputs = [
-      pkgs.haskellPackages.cabal-install
-      pkgs.haskellPackages.ghcid
-      pkgs.haskellPackages.ormolu
-      pkgs.haskellPackages.hlint
-      pkgs.niv
-      pkgs.nixpkgs-fmt
-    ];
-    withHoogle = true;
-  };
-
-  exe = pkgs.haskell.lib.justStaticExecutables (myHaskellPackages."epub2json");
-
-  docker = pkgs.dockerTools.buildImage {
-    name = "epub2json";
-    config.Cmd = [ "${exe}/bin/epub2json" ];
-  };
-in
-{
-  inherit shell;
-  inherit exe;
-  inherit docker;
-  inherit myHaskellPackages;
-  "epub2json" = myHaskellPackages."epub2json";
-}
+(import (
+  let
+    lock = builtins.fromJSON (builtins.readFile ./flake.lock);
+  in fetchTarball {
+    url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
+    sha256 = lock.nodes.flake-compat.locked.narHash; }
+) {
+  src =  ./.;
+}).defaultNix
