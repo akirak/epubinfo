30e81762b1e8a22d69d8e20a6b8b04459f4756ff
Akira Komamura <akira.komamura@gmail.com>
2021-05-01 01:08:56 +0900

    Organize Nix files to follow the classic conventions

--- shell.nix
+++ shell.nix
@@ -1,1 +1,22 @@
-(import ./default.nix {}).shell
+{ pkgs ? import <nixpkgs> { } }:
+let
+  sources = import ./nix/sources.nix;
+  gitignore = import sources."gitignore.nix" { inherit (pkgs) lib; };
+  pre-commit = (import sources."pre-commit-hooks.nix").run {
+    src = gitignore.gitignoreSource ./.;
+    hooks = {
+      nixpkgs-fmt.enable = true;
+      nix-linter.enable = true;
+      ormolu.enable = true;
+      hlint.enable = true;
+      hpack.enable = true;
+    };
+  };
+in
+pkgs.mkShell {
+  buildInputs = [
+    (import ./compat.nix).shellNix
+  ];
+
+  inherit (pre-commit) shellHook;
+}
