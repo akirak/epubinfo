85968064203d97c953d8c3f0472d4e086467b11b
Akira Komamura <akira.komamura@gmail.com>
2022-12-23 19:36:34 +0900

    Simplify flake.nix by following NixOS/templates#haskell-hello

--- flake.nix
+++ flake.nix
@@ -1,51 +1,35 @@
 {
   description = "Extract metadata from an EPUB file";
 
-  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }: (flake-utils.lib.eachDefaultSystem (
+    system: let
 
-  outputs = { nixpkgs, flake-utils, ... }:
-    (flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        gitignore = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ];
-        myHaskellPackages = pkgs.haskell.packages.ghc810.override {
-          overrides = hself: _hsuper: {
-            "epubinfo" =
-              hself.callCabal2nix
-                "epubinfo"
-                (gitignore ./.)
-                { };
-          };
-        };
-      in
-      rec {
-        packages.epubinfo = pkgs.haskell.lib.justStaticExecutables myHaskellPackages.epubinfo;
-        defaultPackage = packages.epubinfo;
-        apps.epubinfo = flake-utils.lib.mkApp {
-          drv = packages.epubinfo;
-        };
-        defaultApp = apps.epubinfo;
-        lib.haskellPackages = myHaskellPackages;
-        lib.epubinfo = lib.haskellPackages.epubinfo;
-        devShell = myHaskellPackages.shellFor {
-          packages = p: [
-            p.epubinfo
-          ];
-          buildInputs = [
-            pkgs.haskellPackages.cabal-install
-            pkgs.haskellPackages.ghcid
-            pkgs.haskellPackages.ormolu
-            pkgs.haskellPackages.hlint
-            pkgs.niv
-            pkgs.nixpkgs-fmt
-          ];
-          withHoogle = true;
-          # TODO: Add shellHook for installing the pre-commit hook
-          # currently defined in setup-hooks.nix
-          #
-          # See https://github.com/cachix/pre-commit-hooks.nix/pull/67
-        };
-      }
-    ));
+      pkgs = nixpkgs.legacyPackages.${system};
 
+      inherit (pkgs) haskellPackages;
+
+      epubinfo = haskellPackages.callCabal2nix "epubinfo" ./. {};
+    in rec {
+      packages = {
+        epubinfo = pkgs.haskell.lib.justStaticExecutables epubinfo;
+        default = self.packages.${system}.epubinfo;
+      };
+      devShell = haskellPackages.shellFor {
+        packages = _p: [
+          epubinfo
+        ];
+        buildInputs = with haskellPackages; [
+          cabal-install
+          ghcid
+          cabal-install
+        ];
+        withHoogle = true;
+      };
+    }
+  ));
 }
