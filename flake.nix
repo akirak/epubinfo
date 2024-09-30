c7c05a7cc08075d53f2d2fcc6458677d5a6d7ddc
Akira Komamura <akira.komamura@gmail.com>
2024-10-01 00:29:52 +0900

    chore: Add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -6,6 +6,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     systems.url = "github:nix-systems/default";
     haskell-flake.url = "github:srid/haskell-flake";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
   };
 
   nixConfig = {
@@ -22,6 +23,7 @@
     flake-parts.lib.mkFlake { inherit inputs; } {
       imports = [
         inputs.haskell-flake.flakeModule
+        inputs.treefmt-nix.flakeModule
       ];
       systems = import inputs.systems;
       perSystem =
@@ -36,6 +38,21 @@
           };
 
           packages.default = self.packages.${system}.epubinfo;
+
+          treefmt = {
+            flakeCheck = true;
+            projectRootFile = "package.yaml";
+            programs = {
+              actionlint.enable = true;
+              ormolu.enable = true;
+              cabal-fmt.enable = true;
+              nixfmt.enable = true;
+              # Disable hlint for now as I don't want to fix the warnings for
+              # now
+              #
+              # hlint.enable = true;
+            };
+          };
         };
     };
 }
